46cb850e3a03dfa20a034c0d3dd2990f33894e75
Daniel <dfdazac@gmail.com>
2018-10-30 14:42:27 +0100

    Initial commit

---
+++ 01-letter-frequencies/count.py
@@ -0,0 +1,19 @@
+import string
+from collections import Counter
+
+# Read data
+with open('Jack.txt') as file:
+    data = file.read().lower()
+
+# Remove punctuation
+alphabet = set(string.ascii_lowercase)
+letters = ''
+for letter in data:
+    if letter in alphabet:
+        letters += letter
+
+# Count letters
+counts = Counter(letters)
+most_common = counts.most_common(1)[0]
+print('Most common letter is {} with {:d} occurrences'.format(most_common[0],
+                                                              most_common[1]))
